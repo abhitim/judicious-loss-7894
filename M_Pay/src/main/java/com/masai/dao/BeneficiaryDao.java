b83e96254303bac2c43f8f512f9dee32973bb368
Rituraj Nagar <riturajnagar78@gmail.com>
2022-11-19 13:13:22 +0530

    Delete M_Pay directory

--- M_Pay/src/main/java/com/masai/dao/BeneficiaryDao.java
+++
@@ -1,9 +0,0 @@
-package com.masai.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.model.BeneficiaryDetails;
-
-public interface BeneficiaryDao extends JpaRepository<BeneficiaryDetails, String>{
-
-}
